5de9df1d57751d8517721a74db9de950ddeeef3c
sodaho <sodaho@mac.com>
2025-11-28 22:29:04 +0100

    feat: add exception classes

---
+++ src/Exception/EnvLoaderException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hd3r\EnvLoader\Exception;
+
+use Exception;
+
+abstract class EnvLoaderException extends Exception
+{
+}
---
+++ src/Exception/MissingRequiredKeyException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hd3r\EnvLoader\Exception;
+
+class MissingRequiredKeyException extends EnvLoaderException
+{
+}
