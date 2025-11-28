c52b6429f916edc0e91a3ec966acdcb5418c06b2
sodaho <sodaho@mac.com>
2025-11-28 22:49:22 +0100

    test: add EnvLoaderTest

---
+++ tests/Unit/EnvLoaderTest.php
@@ -0,0 +1,243 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hd3r\EnvLoader\Tests\Unit;
+
+use Hd3r\EnvLoader\EnvLoader;
+use Hd3r\EnvLoader\Exception\FileNotFoundException;
+use Hd3r\EnvLoader\Exception\FileNotReadableException;
+use Hd3r\EnvLoader\Exception\InvalidKeyException;
+use Hd3r\EnvLoader\Exception\MissingRequiredKeyException;
+use PHPUnit\Framework\TestCase;
+
+class EnvLoaderTest extends TestCase
+{
+    private string $tempDir;
+
+    protected function setUp(): void
+    {
+        $this->tempDir = sys_get_temp_dir() . '/env-loader-test-' . uniqid();
+        mkdir($this->tempDir);
+    }
+
+    protected function tearDown(): void
+    {
+        // Clean up temp files
+        $files = glob($this->tempDir . '/*');
+        foreach ($files as $file) {
+            unlink($file);
+        }
+        rmdir($this->tempDir);
+
+        // Reset $_ENV
+        foreach (array_keys($_ENV) as $key) {
+            if (str_starts_with($key, 'TEST_')) {
+                unset($_ENV[$key]);
+            }
+        }
+    }
+
+    private function createEnvFile(string $content): string
+    {
+        $path = $this->tempDir . '/.env';
+        file_put_contents($path, $content);
+        return $path;
+    }
+
+    public function testLoadsSimpleKeyValue(): void
+    {
+        $path = $this->createEnvFile('TEST_KEY=value');
+        EnvLoader::load($path);
+
+        $this->assertEquals('value', $_ENV['TEST_KEY']);
+    }
+
+    public function testLoadsMultipleKeyValues(): void
+    {
+        $path = $this->createEnvFile("TEST_ONE=first\nTEST_TWO=second");
+        EnvLoader::load($path);
+
+        $this->assertEquals('first', $_ENV['TEST_ONE']);
+        $this->assertEquals('second', $_ENV['TEST_TWO']);
+    }
+
+    public function testLoadsEmptyValue(): void
+    {
+        $path = $this->createEnvFile('TEST_EMPTY=');
+        EnvLoader::load($path);
+
+        $this->assertEquals('', $_ENV['TEST_EMPTY']);
+    }
+
+    public function testLoadsValueWithEqualsSign(): void
+    {
+        $path = $this->createEnvFile('TEST_PASSWORD=val=ue=with=equals');
+        EnvLoader::load($path);
+
+        $this->assertEquals('val=ue=with=equals', $_ENV['TEST_PASSWORD']);
+    }
+
+    public function testLoadsDoubleQuotedValue(): void
+    {
+        $path = $this->createEnvFile('TEST_QUOTED="hello world"');
+        EnvLoader::load($path);
+
+        $this->assertEquals('hello world', $_ENV['TEST_QUOTED']);
+    }
+
+    public function testLoadsSingleQuotedValue(): void
+    {
+        $path = $this->createEnvFile("TEST_SINGLE='hello world'");
+        EnvLoader::load($path);
+
+        $this->assertEquals('hello world', $_ENV['TEST_SINGLE']);
+    }
+
+    public function testLoadsEscapedQuotes(): void
+    {
+        $path = $this->createEnvFile('TEST_ESCAPED="hello \"world\""');
+        EnvLoader::load($path);
+
+        $this->assertEquals('hello "world"', $_ENV['TEST_ESCAPED']);
+    }
+
+    public function testIgnoresCommentLines(): void
+    {
+        $path = $this->createEnvFile("# This is a comment\nTEST_KEY=value");
+        EnvLoader::load($path);
+
+        $this->assertEquals('value', $_ENV['TEST_KEY']);
+        $this->assertArrayNotHasKey('#', $_ENV);
+    }
+
+    public function testHandlesInlineComment(): void
+    {
+        $path = $this->createEnvFile('TEST_INLINE=value # this is a comment');
+        EnvLoader::load($path);
+
+        $this->assertEquals('value', $_ENV['TEST_INLINE']);
+    }
+
+    public function testInlineCommentWithQuotes(): void
+    {
+        $path = $this->createEnvFile('TEST_QUOTED_COMMENT="value with # hash" # comment');
+        EnvLoader::load($path);
+
+        $this->assertEquals('value with # hash', $_ENV['TEST_QUOTED_COMMENT']);
+    }
+
+    public function testTrimsWhitespaceAroundKey(): void
+    {
+        $path = $this->createEnvFile('  TEST_SPACED  =value');
+        EnvLoader::load($path);
+
+        $this->assertEquals('value', $_ENV['TEST_SPACED']);
+    }
+
+    public function testTrimsWhitespaceAroundValue(): void
+    {
+        $path = $this->createEnvFile('TEST_TRIM=  value  ');
+        EnvLoader::load($path);
+
+        $this->assertEquals('value', $_ENV['TEST_TRIM']);
+    }
+
+    public function testIgnoresEmptyLines(): void
+    {
+        $path = $this->createEnvFile("\n\nTEST_EMPTY_LINES=value\n\n");
+        EnvLoader::load($path);
+
+        $this->assertEquals('value', $_ENV['TEST_EMPTY_LINES']);
+    }
+
+    public function testDoesNotOverwriteByDefault(): void
+    {
+        $_ENV['TEST_EXISTING'] = 'original';
+        $path = $this->createEnvFile('TEST_EXISTING=new');
+        EnvLoader::load($path);
+
+        $this->assertEquals('original', $_ENV['TEST_EXISTING']);
+    }
+
+    public function testOverwriteWhenEnabled(): void
+    {
+        $_ENV['TEST_OVERWRITE'] = 'original';
+        $path = $this->createEnvFile('TEST_OVERWRITE=new');
+        EnvLoader::load($path, overwrite: true);
+
+        $this->assertEquals('new', $_ENV['TEST_OVERWRITE']);
+    }
+
+    public function testRequiredKeysAsArray(): void
+    {
+        $path = $this->createEnvFile("TEST_REQ_ONE=one\nTEST_REQ_TWO=two");
+        EnvLoader::load($path, required: ['TEST_REQ_ONE', 'TEST_REQ_TWO']);
+
+        $this->assertEquals('one', $_ENV['TEST_REQ_ONE']);
+        $this->assertEquals('two', $_ENV['TEST_REQ_TWO']);
+    }
+
+    public function testRequiredKeysAsString(): void
+    {
+        $path = $this->createEnvFile("TEST_STR_ONE=one\nTEST_STR_TWO=two");
+        EnvLoader::load($path, required: 'TEST_STR_ONE,TEST_STR_TWO');
+
+        $this->assertEquals('one', $_ENV['TEST_STR_ONE']);
+        $this->assertEquals('two', $_ENV['TEST_STR_TWO']);
+    }
+
+    public function testThrowsFileNotFoundException(): void
+    {
+        $this->expectException(FileNotFoundException::class);
+        EnvLoader::load('/nonexistent/path/.env');
+    }
+
+    public function testThrowsInvalidKeyException(): void
+    {
+        $path = $this->createEnvFile('123INVALID=value');
+        $this->expectException(InvalidKeyException::class);
+        EnvLoader::load($path);
+    }
+
+    public function testThrowsInvalidKeyExceptionForHyphen(): void
+    {
+        $path = $this->createEnvFile('INVALID-KEY=value');
+        $this->expectException(InvalidKeyException::class);
+        EnvLoader::load($path);
+    }
+
+    public function testThrowsMissingRequiredKeyException(): void
+    {
+        $path = $this->createEnvFile('TEST_EXISTS=value');
+        $this->expectException(MissingRequiredKeyException::class);
+        EnvLoader::load($path, required: ['TEST_MISSING']);
+    }
+
+    public function testParseReturnsArrayWithoutSettingEnv(): void
+    {
+        $path = $this->createEnvFile("TEST_PARSE_ONE=one\nTEST_PARSE_TWO=two");
+        $result = EnvLoader::parse($path);
+
+        $this->assertEquals(['TEST_PARSE_ONE' => 'one', 'TEST_PARSE_TWO' => 'two'], $result);
+        $this->assertArrayNotHasKey('TEST_PARSE_ONE', $_ENV);
+        $this->assertArrayNotHasKey('TEST_PARSE_TWO', $_ENV);
+    }
+
+    public function testParseReturnsEmptyArrayForEmptyFile(): void
+    {
+        $path = $this->createEnvFile('');
+        $result = EnvLoader::parse($path);
+
+        $this->assertEquals([], $result);
+    }
+
+    public function testParseReturnsEmptyArrayForOnlyComments(): void
+    {
+        $path = $this->createEnvFile("# Comment one\n# Comment two");
+        $result = EnvLoader::parse($path);
+
+        $this->assertEquals([], $result);
+    }
+
+}
